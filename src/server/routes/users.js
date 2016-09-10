effb1f887c59cafe54a8c192f68806c74494aae1
Alexander Glassford <a.b.glassford@gmail.com>
2016-09-10 10:20:25 -0600

    practice

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const knex = require('../db/knex');
 
+//GET ALL users
 router.get('/', (req, res, next) => {
   knex('users').select('*')
   .then((users) => {
@@ -19,6 +20,7 @@ router.get('/', (req, res, next) => {
   });
 });
 
+//GET SINGLE
 router.get('/:id', (req, res, next) => {
   const userID = parseInt(req.params.id);
   knex('users')
@@ -39,5 +41,72 @@ router.get('/:id', (req, res, next) => {
     });
   });
 });
-
+//add a user
+router.post('/', (req, res, next) => {
+  const newUsername = req.body.username;
+  const newEmail = req.body.email;
+  console.log(newUsername);
+  knex('users')
+  .insert({
+    username: newUsername,
+    email: newEmail
+  })
+  .returning('*')
+  .then((user) => {
+    res.status(201).json({
+      status: 'success',
+      data: user
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err
+    });
+  });
+});
+//update a user
+router.put('/:id', (req, res, next) => {
+  const userId = parseInt(req.params.id);
+  const updatedUsername = req.body.username;
+  const updatedEmail = req.body.email;
+  knex('users')
+  .where('id', userId)
+  .update({
+    username: updatedUsername,
+    email: updatedEmail
+  })
+  .returning('*')
+  .then((user) => {
+    res.status(200).json({
+      status: 'success',
+      data: user
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err
+    });
+  });
+});
+router.delete('/:id', (req, res, next) => {
+  const userId = parseInt(req.params.id);
+  knex('users')
+  .where('id', userId)
+  .del()
+  .returning('*')
+  .then((user) => {
+    res.status(200).json({
+      status: 'success',
+      data: user
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err
+    });
+  });
+});
 module.exports = router;
--- test/integration/routes.users.test.js
+++ test/integration/routes.users.test.js
@@ -81,20 +81,106 @@ describe('routes : users', () => {
     });
   });
 
-  // describe('POST /api/v1/users', () => {
-  //   it('should add a new user', (done) => {
-  //     chai.request(server)
-  //     .post('/api/v1/users')
-  //     .end((err, res) => {
-  //       should.not.exist(err);
-  //       res.redirects.length.should.equal(0);
-  //       res.status.should.equal(200);
-  //       res.type.should.equal('application/json');
-  //       res.body.status.should.eql('success');
-  //       res.body.data.length.should.eql(0);
-  //       done();
-  //     });
-  //   });
-  // });
-
+  describe('POST /api/v1/users/', () => {
+    it('should respond with a success message along with the single user that was added', (done) => {
+      chai.request(server)
+      .post('/api/v1/users')
+      .send({
+        username: 'Jimmy',
+        email: 'jimmyh@jimmy.com'
+      })
+      .end((err, res) => {
+        // there should be no errors
+        should.not.exist(err);
+        // there should be a 201 status code
+        //indicating something was created
+        res.status.should.equal(201);
+        // the response should be JSON
+        res.type.should.equal('application/json');
+        // the JSON response body should have a
+        // key-value pair of {"status": "success"}
+        res.body.status.should.eql('success');
+        // the JSON response body should have a
+        // key-value pair of {"data": 1 user object}
+        res.body.data[0].should.include.keys(
+          'id', 'username', 'email', 'created_at'
+        );
+        done();
+      });
+    });
+  });
+  describe('PUT /api/v1/users/:id', () => {
+    it('should respond with a success message along with the single user that was added', (done) => {
+      knex('users')
+      .select('*')
+      .then((user) => {
+        const userObject = user[0];
+        chai.request(server)
+        .put(`/api/v1/users/${userObject.id}`)
+        .send({
+          username: 'updatedUser',
+          email: 'updated@user.com'
+        })
+        .end((err, res) => {
+          // there should be no errors
+          should.not.exist(err);
+          // there should be a 200 status code
+          //indicating something was created
+          res.status.should.equal(200);
+          // the response should be JSON
+          res.type.should.equal('application/json');
+          // the JSON response body should have a
+          // key-value pair of {"status": "success"}
+          res.body.status.should.eql('success');
+          // the JSON response body should have a
+          // key-value pair of {"data": 1 user object}
+          res.body.data[0].should.include.keys(
+            'id', 'username', 'email', 'created_at'
+          );
+          //ensure the user was in fact updated
+          var newUserObject = res.body.data[0];
+          newUserObject.username.should.not.eql(userObject.username);
+          newUserObject.email.should.not.eql(userObject.email);
+          newUserObject.username.should.eql('updatedUser');
+          newUserObject.email.should.eql('updated@user.com');
+          done();
+        });
+      });
+    });
+  });
+  describe('DELETE /api/v1/users/:id', () => {
+    it('should respond with a success message along with the single user that was deleted', (done) => {
+      knex('users')
+      .select('*')
+      .then((user) => {
+        const userObject = user[0];
+        const lengthBeforeDelete = user.length;
+        chai.request(server)
+        .delete(`/api/v1/users/${userObject.id}`)
+        .end((err, res) => {
+          // there should be no errors
+          should.not.exist(err);
+          // there should be a 200 status code
+          //indicating something was created
+          res.status.should.equal(200);
+          // the response should be JSON
+          res.type.should.equal('application/json');
+          // the JSON response body should have a
+          // key-value pair of {"status": "success"}
+          res.body.status.should.eql('success');
+          // the JSON response body should have a
+          // key-value pair of {"data": 1 user object}
+          res.body.data[0].should.include.keys(
+            'id', 'username', 'email', 'created_at'
+          );
+          //ensure the user was in fact deleted
+          knex('users').select('*')
+          .then((updatedUsers) => {
+            updatedUsers.length.should.eql(lengthBeforeDelete - 1);
+          });
+          done();
+        });
+      });
+    });
+  });
 });
